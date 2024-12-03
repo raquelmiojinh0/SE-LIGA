cd22f1ed65615a3eadfd08333c57246a882cf97a
raquel <110173228+raquelmiojinh0@users.noreply.github.com>
2024-12-03 13:56:55 -0300

    Add files via upload

---
+++ se liga/main.js
@@ -0,0 +1,18 @@
+<!DocumentType html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>exemplo onclick com addeventlistener</title>
+    </head>
+    <body>
+        <script>
+            const botao=
+            documento.getelemenrbyid(`meu botao`)
+            ;
+            botao.addeventlistener(`click`),
+            { function ()
+             alerta(`voce clicou no botao!`) ;
+              }
+        </script>
+    </body>
+</html>
